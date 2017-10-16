0c25ac2664013be06d43bdfef2d0301cf1980b3d
Stanley Yeung <stanley.hk18@gmail.com>
2017-10-16 11:29:45 +0800

    Updated chatroom to include rooms
    rooms for all emotions from API

--- websocket/app.js
+++ websocket/app.js
@@ -13,6 +13,8 @@ app.get('/', (req, res) => {
 var userCount = 0;
 var usernames = {}; //Object variable for storing the usernames currently connected to the chat
 
+var rooms = ['Lobby', 'Angry', 'Contempt', 'Disgust', 'Happy', 'Neutral', 'Sad', 'Surprise'];
+
 io.on('connection', (socket) => {
     //The .on method specifies the event 'connection' we are listening for
     
@@ -22,29 +24,59 @@ io.on('connection', (socket) => {
 
     //Message to print in console when the event 'connection' occurs
     console.log('a user connected to the socket');
+
     //Whenever the client emits 'sendchat', this listens and executes
-    socket.on('sendchat', function(data) {
-        //We tell the client to execute 'updatechat' with 2 parameters.
-        io.sockets.emit('updatechat', socket.username, data);
-    });
+    // socket.on('sendchat', function(data) {
+    //     //We tell the client to execute 'updatechat' with 2 parameters.
+    //     io.sockets.emit('updatechat', socket.username, data);
+    // });
 
 
     //When the client emits 'adduser', the server listens and executes below
     socket.on('adduser', function(username) {
     //First of all, we store the username in the socket session
     socket.username = username;
+
+    socket.room = 'Lobby'; 
+
     // Then add the client's username to the global usernames variable
     usernames[username] = username;
+
+    socket.join('Lobby');
     
     //Update the list of users in the chat on the client's side
     io.sockets.emit('updateusers', usernames);
 
     //Echo to client that they have connected
-    socket.emit('updatechat', 'Server', 'you have connected');
+    socket.emit('updatechat', 'SERVER', 'you have connected to Lobby');
 
     //Broadcast to all other clients that a user has connected
-    socket.broadcast.emit('updatechat', 'Server', username + ' has connected');
+    socket.broadcast.to('Lobby').emit('updatechat', 'SERVER', username + ' has connected to this room');
 
+    socket.emit('updaterooms', rooms, 'Lobby');
+    });
+
+    // socket.on('create', function(room) {
+    //     rooms.push(room);
+    //     socket.emit('updaterooms', rooms, socket.room);
+    // });
+
+    //Only allows chat function to be broadcasted to each specific room
+    socket.on('sendchat', function(data) {
+        io.sockets["in"](socket.room).emit('updatechat', socket.username, data);
+    });
+    
+    
+    socket.on('switchRoom', function(newroom) {
+        var oldroom;
+        oldroom = socket.room;
+        socket.leave(socket.room);
+        socket.join(newroom);
+        socket.emit('updatechat', 'SERVER', 'you have connected to ' + newroom);
+        socket.broadcast.to(oldroom).emit('updatechat', 'SERVER', socket.username + ' has left this room');
+        socket.room = newroom;
+        socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username + ' has joined this room');
+        socket.emit('updaterooms', rooms, newroom);
     });
 
     //Using the .on method to listen on the 'socket' parameter passed in the callback
@@ -59,22 +91,14 @@ io.on('connection', (socket) => {
         io.sockets.emit('updateusers', usernames);
 
         //Broadcast to all other clients that a user has left.
-        socket.broadcast.emit('updatechat', 'Server', socket.username + ' has disconnected');
+        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
          
         //Decreases counter on disconnect and emits another userCount event
         userCount--;
         io.emit('userCount', { userCount: userCount });
 
-        
+        socket.leave(socket.room);
     });
-
-
-    //Listens for 'chat message' event and broadcasts it back to all listening sockets
-    // socket.on('chat message', (msg)=>{
-    //     io.emit('chat message', msg); 
-    // });
-
-
 
 
 });
