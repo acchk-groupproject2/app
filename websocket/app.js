5b4102251b6379d399cecff496bf1c183574146e
Stanley Yeung <stanley.hk18@gmail.com>
2017-10-17 14:33:12 +0800

    Fixed empty username problem and made sure there is a unique username
    The unique username is generated by adding '2'.  There may be a better way of doing this.

--- websocket/app.js
+++ websocket/app.js
@@ -16,57 +16,49 @@ var usernames = {}; //Object variable for storing the usernames currently connec
 var rooms = ['Lobby', 'anger', 'contempt', 'disgust', 'fear', 'happiness', 'neutral', 'sadness', 'surprise'];
 
 io.on('connection', (socket) => {
-    //The .on method specifies the event 'connection' we are listening for
     
     //Increases counter and emits the userCount event
     userCount++;
     io.emit('userCount', {userCount: userCount});
 
-    //Message to print in console when the event 'connection' occurs
-    console.log('a user connected to the socket');
-
-    //Whenever the client emits 'sendchat', this listens and executes
-    // socket.on('sendchat', function(data) {
-    //     //We tell the client to execute 'updatechat' with 2 parameters.
-    //     io.sockets.emit('updatechat', socket.username, data);
-    // });
-
-
     //When the client emits 'adduser', the server listens and executes below
     socket.on('adduser', function(username) {
-    //First of all, we store the username in the socket session
-    socket.username = username;
 
-    socket.room = 'Lobby'; 
-
-    // Then add the client's username to the global usernames variable
-    usernames[username] = username;
-
-    socket.join('Lobby');
+        while (usernames.hasOwnProperty(username)) {
+            username += "2";
+        }
     
-    //Update the list of users in the chat on the client's side
-    io.sockets.emit('updateusers', usernames);
+        //First of all, we store the username in the socket session
+        socket.username = username;
 
-    //Echo to client that they have connected
-    socket.emit('updatechat', 'SERVER', 'you have connected to Lobby');
+        //We also store the room's name in the socket session
+        socket.room = 'Lobby'; 
 
-    //Broadcast to all other clients that a user has connected
-    socket.broadcast.to('Lobby').emit('updatechat', 'SERVER', username + ' has connected to this room');
+        // Then add the client's username to the global usernames variable
+        usernames[username] = username;
 
-    socket.emit('updaterooms', rooms, 'Lobby');
+        //The first room to join on connecting
+        socket.join('Lobby');
+        
+        //Update the list of users in the chat on the client's side
+        io.sockets.emit('updateusers', usernames);
+
+        //Echo to client that they have connected
+        socket.emit('updatechat', 'SERVER', 'you have connected to Lobby');
+
+        //Broadcast to all other clients that a user has connected
+        socket.broadcast.to('Lobby').emit('updatechat', 'SERVER', username + ' has connected to this room');
+
+        //Updates the room list with the current room as 'Lobby'
+        socket.emit('updaterooms', rooms, 'Lobby');
     });
-
-    // socket.on('create', function(room) {
-    //     rooms.push(room);
-    //     socket.emit('updaterooms', rooms, socket.room);
-    // });
 
     //Only allows chat function to be broadcasted to each specific room
     socket.on('sendchat', function(data) {
         io.sockets["in"](socket.room).emit('updatechat', socket.username, data);
     });
     
-    
+    //Listens for 'switchRoom' event from the client then broadcasts the relevant messages and updates room list
     socket.on('switchRoom', function(newroom) {
         var oldroom;
         oldroom = socket.room;
@@ -79,11 +71,7 @@ io.on('connection', (socket) => {
         socket.emit('updaterooms', rooms, newroom);
     });
 
-    //Using the .on method to listen on the 'socket' parameter passed in the callback
-    //Upon the 'disconnect' event, print another message.
     socket.on('disconnect', () => {
-
-        console.log('a user left us');
 
         //Remove the username from the global usernames variable
         delete usernames[socket.username];
@@ -102,13 +90,5 @@ io.on('connection', (socket) => {
 
 
 });
-
-//server side for detect typing
-//   client.on("typing", function(data) {  
-//     if (typeof people[client.id] !== "undefined")
-//     socket.sockets.in(client.room).emit("isTyping", {isTyping: data, person: people[client.id].name});
-//     client.broadcast.to(client.room).emit("isTyping", {isTyping: data, person: people[client.id].name});
-// console.log("Someone is typing");
-// });
 
 http.listen(3030);
